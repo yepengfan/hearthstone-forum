248653e144200c50dbf9ab86e5a56e6ba635f6e2
Yepeng Fan <fanyepeng@yeah.net>
2015-02-14 15:31:33 +1100

    bug free, not disable sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,7 @@ Rails.application.routes.draw do
   get 'page/home'
   get '/:name' => 'management#manage', as: 'user_management'
 
-  devise_for :users, skip: :registrations
-  devise_scope :user do
-    resource :registration,
-      #only: [:new, :create, :edit, :update],
-      only: [:edit, :update],
-      path: 'users',
-      path_names: { new: 'sign_up' },
-      controller: 'devise/registrations',
-
-      # disable user to destroy their account
-      as: :user_registration do
-        get :cancel
-      end
-  end
+  devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
