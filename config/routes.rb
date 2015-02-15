f4f3c30c6ed5194a179827bbe442789c3de423a1
Yepeng Fan <fanyepeng@yeah.net>
2015-02-16 00:29:04 +1100

    open for admin registration

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
   get '/admin/:name' => 'management#manage', as: 'user_management'
 
   devise_for :users
-  as :user do
-    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', :as => 'user_registration'
-  end
+  # as :user do
+  #   get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+  #   put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  # end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
