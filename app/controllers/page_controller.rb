0beaec3b746ef5411d67c8d07860656f412f0a73
Yepeng Fan <fanyepeng@yeah.net>
2015-02-15 22:31:36 +1100

    add home page validation alert

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -7,8 +7,10 @@ class PageController < ApplicationController
   def create_enrolment
     @enrolment = Enrolment.new(:account => enrolment_params[:account], :match_id => enrolment_params[:match_id])
     @enrolment.save
+    @match = Match.where(status: true).first
 
-    redirect_to root_path
+    # redirect_to root_path
+    render "home"
   end
 
   def enrolment_params
--- app/models/enrolment.rb
+++ app/models/enrolment.rb
@@ -1,7 +1,9 @@
 class Enrolment < ActiveRecord::Base
   has_many :matches
   # validate account with Chinese, English and Korean
-  validates :account, presence: true, length: { minimum: 8, maximum: 20 }, format: { with: /([A-Za-z\u4e00-\u9fa5\u2E80-\u9FFF\uac00-\ud7ff]{3,12}#\d{4,10})/ }
+  validates :account, :presence => { :message => "canot be blank!!!"},
+                      :length => { :within => 6..20, :message => "must be realistic!" },
+                      :format => { with: /([A-Za-z\u4e00-\u9fa5\u2E80-\u9FFF\uac00-\ud7ff]{3,12}#\d{4,10})/, :message => "format should be like [Account#BattleTag] !!!" }
 
   def export_enrolment(m_id)
     list = Enrolment.where(:match_id => m_id)
